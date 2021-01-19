be1fbaddc220fcdb34f5c1be383f6695dee8df7e
mggzmn <mggzmn91@gmail.com>
2021-01-19 11:31:04 -0600

    Accordion Component

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -3,15 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-mods-home',
   templateUrl: './mods-home.component.html',
-  styleUrls: ['./mods-home.component.css']
+  styleUrls: ['./mods-home.component.css'],
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
-  constructor() { }
+  items = [
+    { title: 'Some Question', content: 'Some answer' },
+    { title: 'Some Question', content: 'Some answer' },
+    { title: 'Some Question', content: 'Some answer' },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-  onClick(){
+  ngOnInit(): void {}
+  onClick() {
     this.modalOpen = !this.modalOpen;
   }
 }
